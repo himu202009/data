4f8ded1408a34363bc5efa70bcd10da95103bd10
Md Mehrab Hossain Himu <120010735+himu202009@users.noreply.github.com>
2026-08-18 11:09:15 +0600

    Rename 'Transaction ID' to 'Access Code'

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const SITE_CONFIG = {
     phone: "Phone",
     institution: "Institution",
     hscBatch: "HSC Batch",
-    transactionId: "Transaction ID",
+    transactionId: "Access Code",
   },
 
   // Shown when a field has no value
